725767bac35c651b37717dcd1452009d1ce7ba67
21327 <rodrigo.villegaszambrano@vtsd.com>
2022-10-12 07:33:28 -0400

    ok

--- Lab04avst.java
+++ Lab04avst.java
@@ -5,23 +5,32 @@
 
 public class Lab04avst
 {
-    public static void main(String[] args)
+    public static void main (String[] args)
     {
-        System.out.println("Lab04a, Student Version\n");
+            System.out.println("Lab04a, Student Version\n");
 
-        double principal  = 250000;
-        double annualRate = 4.85;
-        double numYears   = 30;
+            double principal = 250000;
+            double annualRate = 4.85;
+            double numYears = 30;
+            double numMonths = numYears * 12;
+            // Convert to decimal, then months
+            double monthlyRate = (annualRate / 100) /12;
+            double numeratorTwo = Math.pow(1 + monthlyRate, numMonths);
+            double numerator = monthlyRate * numeratorTwo;
+            double denominator = Math.pow(1 + monthlyRate, numMonths) - 1;
+            double monthPay = (numerator % denominator) * principal;
 
 
 
 
+            System.out.println("Principal:           $" + principal);
+            System.out.println("Annual Rate           " + annualRate + "%");
+            System.out.println("Number of Years       " + numYears);
+            System.out.println("Monthly Payment      $" + monthPay);
 
 
 
 
-
-        System.out.println();
-    }
+        }
 }
 
---
+++ test.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.applet.*;
+
+public class test {
+    public void paint(Graphics g){
+        g.drawLine(50,50,100,100);
+        g.drawLine(100, 100, 150,150);
+        
+
+    }
+
+}
